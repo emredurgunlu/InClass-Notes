bb66b731372398765a9a85cfaa2c48bddacf21c2
emre durgunlu <emredurgunlu@gmail.com>
2023-11-30 23:27:07 +0300

    update

--- react_notes/react_1/giris/src/Footer.jsx
+++ react_notes/react_1/giris/src/Footer.jsx
@@ -1,21 +1,22 @@
 // import React from 'react' bu satırı react version 17 sonrası için sil
 
 // rafc emmet //* Named export ile paylasima acildi import { Main } from "./Main"
+// tüm react kısayolları için dsznajder extensionı > details > snippets
 // export const Footer = () => {
 //   return (
 //     <div>Footer</div>
 //   )
 // }
 
-//*   JS alani yorum satırı
 const mesaj = "hello";
-console.log(mesaj); // Jsx alanında console çalışmaz
+console.log("JS alani", mesaj);
 
 // rafce emmet //* Default export ile paylasima acildi import Footer from "./Footer"
 const Footer = () => {
   return (
     <>
-      {/* JSX alani yorum satırı*/}
+      {true ? console.log("JSX alani", mesaj) : console.log("false")}
+      {/* JS ifadeleri JSX alanında süslü parantez içinde yazılır*/}
       <footer className="my-footer">Harici CSS Footer {mesaj} </footer>
     </>
   );
