8057685d8123f181938edb94ae5a2440c4dc9f5f
emre durgunlu <emredurgunlu@gmail.com>
2023-12-04 16:50:09 +0300

    update

--- react_notes/react_1/giris/src_props/components/Card.jsx
+++ react_notes/react_1/giris/src_props/components/Card.jsx
@@ -1,6 +1,10 @@
 import Photo from "./Photo";
-
-const Card = ({ name, tel, src }) => {
+// props için bak:
+// https://github.com/emredurgunlu/Products-List/blob/master/src/components/products/ProductsList.jsx
+// Bunu aşağıdaki linkte arat (bu dosyada allta children kullanımına örnek de var): 
+// When you nest content inside a JSX tag, the parent component will receive that content in a prop called children
+// https://react.dev/learn/passing-props-to-a-component
+const Card = ({ name, tel = 512, src }) => { // tel = 512 değer gelmezse default value
   return (
     <div>
       <h2>Hello {name}</h2>
@@ -33,3 +37,38 @@ const Card = ({ name, tel, src }) => {
 // };
 
 export default Card;
+
+/* -------------------------------------------------------------------------- */
+/*                         children kullanımına örnek                         */
+/* -------------------------------------------------------------------------- */
+// function Card({ children, title }) {
+//   return (
+//     <div className="card">
+//       <div className="card-content">
+//         <h1>{title}</h1>
+//         {children}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default function Profile() {
+//   return (
+//     <div>
+//       <Card title="Photo">
+//         <img
+//           className="avatar"
+//           src="https://i.imgur.com/OKS67lhm.jpg"
+//           alt="Aklilu Lemma"
+//           width={100}
+//           height={100}
+//         />
+//       </Card>
+//       <Card title="About">
+//         <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+//       </Card>
+//     </div>
+//   );
+// }
+
+
