6a1eef826b53fb59265c1c52bf9fab29ffe86fd2
emre durgunlu <emredurgunlu@gmail.com>
2023-12-08 16:35:56 +0300

    update

--- react_notes/react_1/giris/src_props/components/Callback.jsx
+++ react_notes/react_1/giris/src_props/components/Callback.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Callback = ({ handleChange }) => {
-  //  handleChange("emre_callback_deneme"); Fonksyonu burada çağırırsan hata verir
+  //  handleChange("emre_callback_deneme"); Fonksyonu burada çağırırsan hata verir ama fonksyonun içinde setState yoksa hata vermez
   return (
     <div onClick={() => handleChange("emre_callback_deneme")}>Callback</div>
   );
