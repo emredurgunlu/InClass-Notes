9a5469249ebf78d3e5f143c47c54a149dfd7429a
emre durgunlu <emredurgunlu@gmail.com>
2024-01-08 19:40:37 +0300

    update

---
+++ react_notes/react_app/src_router_advanced/App.js
@@ -0,0 +1,51 @@
+import Footer from "./components/Footer"
+import Nav from "./components/Nav"
+import Contact from "./pages/Contact"
+import Home from "./pages/Home"
+import People from "./pages/People"
+import NotFound from "./pages/NotFound"
+import Paths from "./pages/Paths"
+import PersonDetail from "./pages/PersonDetail"
+import Fullstack from "./pages/Fullstack"
+import Aws from "./pages/Aws"
+import Frontend from "./pages/Frontend"
+import Backend from "./pages/Backend"
+import Login from "./pages/Login"
+import PrivateRouter from "./pages/PrivateRouter"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/people" element={<PrivateRouter />}>
+          <Route index path="" element={<People />} />
+          <Route path=":idx" element={<PersonDetail />} />
+        </Route>
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/contact" element={<Contact />} />
+
+        <Route path="/paths" element={<Paths />}>
+          <Route path="fs" element={<Fullstack />}>
+            <Route path="frontend" element={<Frontend />} />
+            <Route path="backend" element={<Backend />} />
+          </Route>
+          <Route index path="" element={<Aws />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ react_notes/react_app/src_router_advanced/pages/PrivateRouter.jsx
@@ -0,0 +1,17 @@
+import { Outlet, Navigate } from "react-router-dom"
+import Login from "./Login"
+
+const PrivateRouter = () => {
+  //? Context API, redux, localStorage
+  const isAuthenticated = true
+
+  //? useNavigate (Hook) ile Navigate(Component) aslında aynı işi yapar. Yani yönlendirme işlemini gerçekleştirir.
+  //? Aralarindaki fark kullanım yerleridir. Hook olan bir event ya da useEefect hook'u nun içerisi gibi yerlerde kullanılır. (Fonkisyionların kullanıldığı yerler)
+
+  //? Diğeri ise bir component olduğu için JSX içeriside yönlendirme yapmak gerektiği durumlarda kullanılır.
+
+  //? Link ise kullanıcıdan bir girdi beklenildiği durumlarda kullanılaiblir.
+  return <div>{isAuthenticated ? <Outlet /> : <Navigate to="/login" />}</div>
+}
+
+export default PrivateRouter
